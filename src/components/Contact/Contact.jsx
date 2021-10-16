da260abcb37dad930df8ed44829047285bf062cf
Aldo Navarrete <aaldiitoo@gmail.com>
2021-10-15 22:23:37 -0500

    deleted method

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -53,7 +53,7 @@ export default function Contact() {
             </div>
             <div className="right">
                 <h2>Contact me <span>and let's work together!</span></h2>
-                <form onSubmit={handleSubmit} data-netlify="true" name="contact" method = "POST">
+                <form onSubmit={handleSubmit} data-netlify="true" name="contact">
                     <input name="email"type="email" placeholder="Email"/>
                     <textarea name="text" className="form-control" placeholder="Leave your message"/>
                     <button name="submit"type="submit" >Send</button>
