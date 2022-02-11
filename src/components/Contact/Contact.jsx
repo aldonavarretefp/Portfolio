9522e10ae68d14f0a23052f58e89f7d2fe64a0ab
Navarrete <aaldiitoo@gmail.com>
2022-02-10 23:26:35 -0600

    features various

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,6 +3,7 @@ import { ReactComponent as DealSvg } from '../../icons/undraw_business_deal_cpi9
 import Swal from 'sweetalert2';
 
 import "./contact.scss";
+import { useForm } from '../../hooks/useForm';
 
 
 const encode = (data) => {
@@ -11,52 +12,62 @@ const encode = (data) => {
         .join("&");
 }
 export default function Contact() {
-    const handleSubmit = async (e) => {
-        fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contact", ...this.state })
-        })
-        .then(() => alert("Success!"))
-        .catch(error => alert(error));
-        e.preventDefault();
-        if(!e.target.email.value){
-            const { value:email } = await Swal.fire({
-                title: 'Email is missing!',
-                input: 'email',
-                inputLabel: 'Your email address',
-                inputPlaceholder: 'Enter your email address'
-            });
-            if(!email){
-                return;    
-            }
-            e.target.email.value = email;
-        } 
-        if(!e.target.text.value){
-            const { value: text } = await Swal.fire({
-                input: 'textarea',
-                inputLabel: 'Your message is missing...!',
-                inputPlaceholder: 'Type your message here...',
-                inputAttributes: {
-                    'aria-label': 'Type your message here'
-                },
-                showCancelButton: true
-            });
-            if(!text){
-                return;    
-            }
-            e.target.text.value = text;
-        }
+
+    const [formValues, handleInputChange] = useForm({
+        email: "",
+        msg: ""
+    });
+    const { email, msg } = formValues;
     
+    const handleSubmit = async (e) => {
+        e.preventDefault();
         Swal.fire({
             position: 'center',
-            icon: 'info',
+            icon: 'success',
             title: "There you go!",
             text: "I will be notified about your message, Thank you! .",
             showConfirmButton: true,
             timer:2500
-            
         });
+
+
+
+        // fetch("/", {
+        //     method: "POST",
+        //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        //     body: encode({ "form-name": "contact", ...this.state })
+        // })
+        // .then(() => alert("Success!"))
+        // .catch(error => alert(error));
+        // e.preventDefault();
+        // if(!e.target.email.value){
+        //     const { value:email } = await Swal.fire({
+        //         title: 'Email is missing!',
+        //         input: 'email',
+        //         inputLabel: 'Your email address',
+        //         inputPlaceholder: 'Enter your email address'
+        //     });
+        //     if(!email){
+        //         return;    
+        //     }
+        //     e.target.email.value = email;
+        // } 
+        // if(!e.target.text.value){
+        //     const { value: text } = await Swal.fire({
+        //         input: 'textarea',
+        //         inputLabel: 'Your message is missing...!',
+        //         inputPlaceholder: 'Type your message here...',
+        //         inputAttributes: {
+        //             'aria-label': 'Type your message here'
+        //         },
+        //         showCancelButton: true
+        //     });
+        //     if(!text){
+        //         return;    
+        //     }
+        //     e.target.text.value = text;
+        // }
+    
     }
 
     return (
@@ -67,9 +78,17 @@ export default function Contact() {
             <div className="right">
                 <h2>Contact me <span>and let's work together!</span></h2>
                 <form onSubmit={handleSubmit} data-netlify="true" name="contact" method= "POST">
-                    <input type="hidden" name="form-name" value= "contact" />
-                    <input name="email"type="email" placeholder="Email"/>
-                    <textarea name="text" className="form-control" placeholder="Leave your message"/>
+                    <input type="hidden" name="form-name" value="contact" />
+                    <div className="inputContainer">
+                        <input name="email" autoComplete="off" type="email" value={email} onChange={handleInputChange} placeholder=" " required/>
+                        <label>Email</label>
+                    </div>
+                    <div className="textAreaContainer">
+                        <textarea name="msg" className="form-control" value={msg} onChange={handleInputChange} placeholder=" " required/>
+                        <label>Leave a message!</label>
+                    </div>
+                        {/* <textarea name="text" className="form-control" placeholder=" "/>
+                        <label>Leave a message!</label> */}
                     <button className="btn-5" name="submit"type="submit" >Send</button>
                 </form>
             </div>
--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -3,7 +3,7 @@ import EmailIcon from '@material-ui/icons/Email';
 import WhatsAppIcon from '@material-ui/icons/WhatsApp';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
-
+import DescriptionIcon from '@material-ui/icons/Description';
 import { Sling as Hamburger } from 'hamburger-react'
 
 
@@ -11,31 +11,43 @@ import "./topBar.scss";
 
 
 export default function TopBar({isOpen,setOpen}) {
+    
     return (
         <div className={isOpen ? "topBar active" : "topBar"}>
             <div className="wrapper">
                 <div className="left">
-                    <a href="#intro" className="logo">Aldo <code>Navarrete</code> </a> 
+                    <a href="#intro" className="logo">Aldo <code>Navarrete</code> </a>
+                    <a href="/files/aldo-navarrete-resume.pdf" target="_blank" rel='noopener noreferrer' ><span>CV</span></a>
                     <div className="itemContainer">
-                        <EmailIcon className="icon"/>
-                        <span>nazamaldoyael@gmail.com</span>
+                        <a href="/files/aldo-navarrete-resume.pdf" without target="_blank" rel='noopener noreferrer' >
+                            <DescriptionIcon className="icon" />
+                        </a>
+                    </div>
+                    <span>Let's connect !</span>
+                    <div className="itemContainer">
+                        <a 
+                            href="mailto:nazamaldoyael@gmail.com?subject=Contact%20for%20Information.&body=Hello Aldo!%20I%20contacted%20you%20to%20ask%20for%20your%20development%20service!%20:)"
+                        >
+                            <EmailIcon className="icon"/>
+                        </a>
+                        {/* <span><a href="mailto:nazamaldoyael@gmail.com?subject=Contact%20for%20Information.&body=Hello Aldo!%20I%20contacted%20you%20to%20ask%20for%20your%20development%20service!%20:)">nazamaldoyael@gmail.com</a></span> */}
                     </div>
                     <div className="itemContainer">
-                        <WhatsAppIcon className="icon"/>
-                        <span>55 78 59 07 24</span>
+                        <a href="https://wa.link/ty2y5a" target="_blank" rel="noopener noreferrer">
+                            <WhatsAppIcon className="icon"/>
+                        </a>
+                        {/* <span><a href="https://wa.link/ty2y5a" target="_blank" rel="noopener noreferrer">55 78 59 07 24</a></span> */}
                     </div>
-                    <div id="iconContainer" className="itemContainer">
-                        <div id="icon">
+                    <div className="itemContainer">
                             <a  rel="noreferrer" target="_blank" href="https://github.com/aldonavarretefp">
                             <GitHubIcon className="icon"/>
                             </a>
-                        </div>
-                        <div id="icon">
-                            <a  rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/aldo-navarrete-zamora-756bb3200/">
-                                <LinkedInIcon className="icon" id="linkedInIcon"/>
-                            </a>
-                        </div>
                     </div>
+                            <div className="itemContainer">
+                                <a  rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/aldo-navarrete-zamora-756bb3200/">
+                                    <LinkedInIcon className="icon" id="linkedInIcon"/>
+                                </a>
+                            </div>
                 </div>
                 
                 {/* Right */}
